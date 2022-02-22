82fd82dcc565a11b534f910c578b4501deb6abcc
Pooja Sree <you@example.com>
2022-02-23 00:35:48 +0530

    Added:calculation of length file

--- src/com/blz/linecomparsn/Linecomparsn.java
+++ src/com/blz/linecomparsn/Linecomparsn.java
@@ -1,7 +1,27 @@
 package com.blz.linecomparsn;
 
+import java.util.Scanner;
+
 public class Linecomparsn {
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparision computation");
+        double x1, x2, y1, y2;
+        double distance1;
+        Scanner scan= new Scanner(System.in);
+        x1 = 0;
+        y1 = 0;
+        x2 = 0;
+        y2 = 0;
+        System.out.println("enter x1 coordinates");
+        x1 = scan.nextInt();
+        System.out.println("enter y1 coordinates");
+        y1 = scan.nextInt();
+        System.out.println("enter x2 coordinates");
+        x2 = scan.nextInt();
+        System.out.println("enter y2 coordinates");
+        x2 = scan.nextInt();
+        System.out.println();
+        distance1 = Math.sqrt(Math.pow(x1 - x2, 2) + (Math.pow(y1 - y2, 2)));
+        System.out.println("The distance" + distance1);
+
     }
 }
