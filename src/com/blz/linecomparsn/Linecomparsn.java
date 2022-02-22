c6ac5a069eca89efc4e771bf1b12df03963359ad
Pooja Sree <you@example.com>
2022-02-23 00:51:49 +0530

    Added:checking equality of lines

--- src/com/blz/linecomparsn/Linecomparsn.java
+++ src/com/blz/linecomparsn/Linecomparsn.java
@@ -23,5 +23,24 @@ public class Linecomparsn {
         distance1 = Math.sqrt(Math.pow(x1 - x2, 2) + (Math.pow(y1 - y2, 2)));
         System.out.println("The distance" + distance1);
 
+        //case-2 line-2 check equality of lines
+        System.out.println();
+        double p1, p2, q1, q2;
+        x1 = 0;
+        y1 = 0;
+        x2 = 0;
+        y2 = 0;
+        System.out.println("enter p1 coordinates");
+        p1 = scan.nextInt();
+        System.out.println("enter q1 coordinates");
+        q1 = scan.nextInt();
+        System.out.println("enter p2 coordinates");
+        p2 = scan.nextInt();
+        System.out.println("enter q2 coordinates");
+        q2 = scan.nextInt();
+        double distance2 = Math.sqrt(Math.pow(p1 - p2, 2) + (Math.pow(q1 - q2, 2)));
+        System.out.println("The distance" + distance2);
+        scan.close();
+
     }
 }
