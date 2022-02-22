02ff0a3d72d5eea3fae433f0efe601257febd9ed
Pooja Sree <you@example.com>
2022-02-23 00:20:44 +0530

    Added:welcome message

---
+++ src/com/blz/linecomparsn/Linecomparsn.java
@@ -0,0 +1,7 @@
+package com.blz.linecomparsn;
+
+public class Linecomparsn {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparision computation");
+    }
+}
---
+++ src/com/blz/linecomparsn/Main.java
@@ -0,0 +1,8 @@
+package com.blz.linecomparsn;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
