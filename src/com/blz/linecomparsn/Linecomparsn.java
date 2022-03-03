0742fb40a3041a7ea100d040de511c432d74ce60
Pooja Sree <you@example.com>
2022-03-04 00:11:32 +0530

    Added:UC3coparatorfor lines difference

--- src/com/blz/linecomparsn/Linecomparsn.java
+++ src/com/blz/linecomparsn/Linecomparsn.java
@@ -3,49 +3,34 @@ package com.blz.linecomparsn;
 import java.util.Objects;
 
 public class Linecomparsn {
-    //Declaring and Initializing the variables
-        static double x1 = 2;
-        static double x2 = 4;
-        static double y1 = 6;
-        static double y2 = 12;
-        static double x3 = 2;
-        static double x4 = 4;
-        static double y3 = 6;
-        static double y4 = 10;
-        String lengthOfLine2 = " ";
-        String lengthOfLine1 = " ";
 
     public static void main(String[] args) {
+    //Declaring and Initializing the variables
+         double x1 = 2;
+         double x2 = 4;
+         double y1 = 6;
+         double y2 = 12;
+         double x3 = 2;
+         double x4 = 4;
+         double y3 = 6;
+         double y4 = 10;
 
-
-        Linecomparsn method = new Linecomparsn();
-        method.lengthCalculation(); // Call method to calculate lengths of two lines
-        method.useEqualsMethod(); // Call method to check equality of lines
-        method.useCompareToMethod(); // Call method to compare length
-    }
-
-
-    private void lengthCalculation() {
         //Use Math function to calculate length of line
-        lengthOfLine1 = String.valueOf(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
-        lengthOfLine2 = String.valueOf(Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2)));
+        String lengthOfLine1 = String.valueOf(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+        String lengthOfLine2 = String.valueOf(Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2)));
         System.out.println("Length Of Line1 = " + lengthOfLine1);
         System.out.println("Length Of Line2 = " + lengthOfLine2);
-    }
 
-    private void useEqualsMethod() {
         //Use Equals method to check equality of two lines
-        if (lengthOfLine1.equals(lengthOfLine2)) {
+        if ( lengthOfLine1.equals(lengthOfLine2) ) {
             System.out.println("Length of two lines are equal");
         } else {
             System.out.println("Length of two lines are different");
         }
-    }
-
-    private void useCompareToMethod() {
 
         //Using compareTo method compare two lines
         double diff = (lengthOfLine1.compareTo(lengthOfLine2));
         System.out.println("Differance between two lines = " + diff);
+
     }
 }
